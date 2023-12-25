686923057a8c167a5c62b3b8ab9333c63a7246e6
Natallia Mazur <natashamazur@bk.ru>
2023-12-25 23:05:51 +0300

    Homework project has been created

---
+++ TMSHomework-Lesson11/TMSHomework-Lesson11/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TMSHomework-Lesson11
+//
+//  Created by Наталья Мазур on 25.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+/*
+ Сверстать калькулятор на UIStackView
+ Уменьшить шрифт при наборе от 6 цифр
+ 
+ *********************
+ При умножении больших цифр выводить как на айфоне число в степени
+ Деление на ноль обработать
+ Вместо "+-" написать вычисление корня
+ 
+ */
